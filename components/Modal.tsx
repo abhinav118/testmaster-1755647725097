aabfb0ff05eece38311b00d02764db1a3af0cc36
Abhi <abhinav118@gmail.com>
2025-08-19 19:55:33 -0400

    Add components/Modal.tsx

---
+++ components/Modal.tsx
@@ -0,0 +1,13 @@
+interface ModalProps {
+  open?: any
+  title?: any
+  size?: any
+}
+
+export default function Modal({ open, title, size }: ModalProps) {
+  return (
+    <div className="">
+      {/* Modal component content */}
+    </div>
+  )
+}
