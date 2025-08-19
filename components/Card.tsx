1808ff647e2ced5d074ac37903bd4969c66b7b7d
Abhi <abhinav118@gmail.com>
2025-08-19 19:55:33 -0400

    Add components/Card.tsx

---
+++ components/Card.tsx
@@ -0,0 +1,13 @@
+interface CardProps {
+  title?: any
+  subtitle?: any
+  actions?: any
+}
+
+export default function Card({ title, subtitle, actions }: CardProps) {
+  return (
+    <div className="">
+      {/* Card component content */}
+    </div>
+  )
+}
