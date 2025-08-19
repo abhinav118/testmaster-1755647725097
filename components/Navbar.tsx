ec42f6e946fa7ab4e4c925392fc5dc6570cbe0b9
Abhi <abhinav118@gmail.com>
2025-08-19 19:55:34 -0400

    Add components/Navbar.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+interface NavbarProps {
+  brand?: any
+  sticky?: any
+}
+
+export default function Navbar({ brand, sticky }: NavbarProps) {
+  return (
+    <div className="">
+      {/* Navbar component content */}
+    </div>
+  )
+}
