64703020012a724750b5f69a4b467e37db9f810b
Abhi <abhinav118@gmail.com>
2025-08-19 19:55:33 -0400

    Add components/Input.tsx

---
+++ components/Input.tsx
@@ -0,0 +1,13 @@
+interface InputProps {
+  type?: any
+  placeholder?: any
+  required?: any
+}
+
+export default function Input({ type, placeholder, required }: InputProps) {
+  return (
+    <div className="">
+      {/* Input component content */}
+    </div>
+  )
+}
