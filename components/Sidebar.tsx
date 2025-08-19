e9a9d24759847422976d645543ba441e09add5e8
Abhi <abhinav118@gmail.com>
2025-08-19 19:55:34 -0400

    Add components/Sidebar.tsx

---
+++ components/Sidebar.tsx
@@ -0,0 +1,12 @@
+interface SidebarProps {
+  collapsed?: any
+  position?: any
+}
+
+export default function Sidebar({ collapsed, position }: SidebarProps) {
+  return (
+    <div className="">
+      {/* Sidebar component content */}
+    </div>
+  )
+}
